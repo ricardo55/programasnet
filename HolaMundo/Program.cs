d7dea61f49f9882f0fae510aacf4965e14d733fc
Ricardo VB <ricardovb@MacBook-Pro-de-Ricardo.local>
2019-01-31 11:45:34 -0600

    Agrego los dos proyectos

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre="";
+            Console.Write("Dame tu nombre:");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine($"Hello World! {nombre}");
+        }
+    }
+}
---
+++ PagaTrabajador/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PagaTrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Clear();
+            
+
+            String nombre;
+            int horas;
+            float paga,tasa=0.1f;
+            float pagaBruta,impuesto,pagaNeta;
+
+            Console.WriteLine("Programa que Calcula la paga de un Trabajador \n");
+
+            Console.Write("Nombre del trabajador: \n");
+            nombre=Console.ReadLine();
+
+            Console.Write("Horas Trabajadas: \n");
+            horas=int.Parse(Console.ReadLine());
+
+            Console.Write("Paga por Hora: \n");
+            paga=int.Parse(Console.ReadLine());
+
+            //Proceso
+            pagaBruta = horas * paga;
+            impuesto = pagaBruta * tasa;
+            pagaNeta = pagaBruta - impuesto;
+
+            //Salida
+
+            Console.Write($"El Trabajador {nombre} \n");
+            Console.Write($"Trabajo {horas} horas, se le pago {paga} pesos por hora\n");
+            Console.Write($"Se tiene un impuesto de {impuesto} \n");
+            Console.Write($"Se tiene una paga Bruta de {pagaBruta} \n");
+            Console.Write($"Se tiene una paga Neta de {pagaNeta} \n");
+
+           
+            
+
+
+
+
+
+        }
+    }
+}
